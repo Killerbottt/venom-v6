7191dd667ab1eb163732863377b55fdb9b1e7f0f
Killerbottt <glxglicxy@gmail.com>
2024-09-27 22:33:44 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7520325135:AAFEysu5q4YfZNlsdSketSckSZkEbmbkskI'
+ADMIN_ID = '5601202744'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
